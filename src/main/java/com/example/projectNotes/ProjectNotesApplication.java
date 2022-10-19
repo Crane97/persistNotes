8b206dc427f902976922066853b614241da53b98
Ruiz <jorge.ruiz@solera.com>
2022-10-19 09:27:48 +0200

    First commit

---
+++ src/main/java/com/example/projectNotes/ProjectNotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectNotesApplication.class, args);
+	}
+
+}
